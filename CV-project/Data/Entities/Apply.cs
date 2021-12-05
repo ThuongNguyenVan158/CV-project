b26011031bc6cce428f8dbfe29e6372aad8a969d
ThuongNguyenVan158 <80138135+ThuongNguyenVan158@users.noreply.github.com>
2021-12-05 22:42:58 +0700

    init project

---
+++ CV-project/Data/Entities/Apply.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace CV_project.Data.Entities
+{
+    [Table("Apply")]
+    public partial class Apply
+    {
+        [Key]
+        [Column("ApplicantID")]
+        public int ApplicantId { get; set; }
+        [Key]
+        [Column("CompanyID")]
+        public int CompanyId { get; set; }
+
+        [ForeignKey(nameof(ApplicantId))]
+        [InverseProperty("Applies")]
+        public virtual Applicant Applicant { get; set; }
+        [ForeignKey(nameof(CompanyId))]
+        [InverseProperty("Applies")]
+        public virtual Company Company { get; set; }
+    }
+}
