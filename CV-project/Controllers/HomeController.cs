c6a853d1ddf9d04984bc1a1a974eda86d118701c
JeFlame <hoangvu130301@gmail.com>
2021-12-12 18:12:03 +0700

    Add Sign_In View in Controller

--- CV-project/Controllers/HomeController.cs
+++ CV-project/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace CV_project.Controllers
             return View();
         }
 
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
