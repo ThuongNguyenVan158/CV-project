6a672f74fe1fd9ef00ac51ed926096de5b25edcb
ThuongNguyenVan158 <80138135+ThuongNguyenVan158@users.noreply.github.com>
2022-01-04 14:40:52 +0700

    View List Job Per Company & Update Job logic

--- CV-project/Models/JobViewModel.cs
+++ CV-project/Models/JobViewModel.cs
@@ -7,6 +7,7 @@ namespace CV_project.Models
 {
     public class JobViewModel
     {
+        public Guid JobId { get; set; }
         public string Name { get; set; }
         public string Position { get; set; }
         public string Information { get; set; }
